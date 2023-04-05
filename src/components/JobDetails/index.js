cda4f9fcd96e76f7450913d58a91adb52b72530f
Yuheng Ding <yuhengding0223@gmail.com>
2023-04-04 19:18:29 -0500

    updated job details page

--- src/components/JobDetails/index.js
+++ src/components/JobDetails/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function JobDetails({ job }) {
+  function handleRedirect() {
+    // window.location.href = job.link;
+    window.open(job.link);
+  }
   return (
     <div class="job-details-container">
       <a href={job.link}>{job.title}</a>
       <h3>{job.company}</h3>
       <h4>{job.location}</h4>
       <h5>{job.postedDate}</h5>
+      <button onClick={handleRedirect}>Apply</button>
+      <p align="left">{job.description}</p>
     </div>
   );
 }
--- src/components/JobThumbnail/index.js
+++ src/components/JobThumbnail/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./styles.css";
 
 export default function JobThumbnail({ job, onClick }) {
   return (
-    <div onClick={onClick}>
-      <h4>{job.title}</h4>
-      <h4>{job.company}</h4>
-      <h4>{job.industry}</h4>
+    <div class="thumbnail-container" onClick={onClick}>
+      <h4 class="t">{job.title}</h4>
+      <h5 class="t">{job.company}</h5>
+      <h5 class="t">Industry: {job.industry}</h5>
     </div>
   );
 }
